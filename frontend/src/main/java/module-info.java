36a4301280a801e9e611545ddc0d493e116c0afa
Rafi <rafisdevelopment@gmail.com>
2026-03-03 09:59:56 +0100

    deleted fxml
    deleted fxml and added the same as java code (but the position of the label is wrong)

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.miracloud.frontend {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.miracloud.frontend to javafx.fxml;
--- frontend/src/main/java/org/miracloud/frontend/HelloApplication.java
+++ frontend/src/main/java/org/miracloud/frontend/HelloApplication.java
@@ -1,19 +1,34 @@
 package org.miracloud.frontend;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Button helloButton = new Button("Click me");
+        Label label = new Label("");
+        StackPane mainPane = new StackPane(helloButton, label);
+        Scene scene = new Scene(mainPane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
+        helloButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //System.out.println("Hello World!");
+                label.setText("Hello");
+            }
+        });
     }
 }
