e022ae42a97851bc204a9a9dfebf50a94adaacd3
Rafi <rafisdevelopment@gmail.com>
2026-03-05 17:56:59 +0100

    added checks for signup and tells the user the error
    the input fields shoulnt be emtpy
    
    the email needs @ and .

--- frontend/src/main/java/org/miracloud/frontend/controller/signupController.java
+++ frontend/src/main/java/org/miracloud/frontend/controller/signupController.java
@@ -7,13 +7,35 @@ public class signupController {
     public String handleSignup(String email, String username, String password, Boolean isChecked){
         System.out.println("email: " + email + "\nusername: " + username + "\npassword: " + password + "\nisChecked: " + isChecked);
 
-        if(!isChecked){
-            return "PLEASE ACCEPT OUR PRIVACY POLICY AND COOKIES";
+        String returnString = "";
+
+        // check if inputs are correct
+        if(!email.isEmpty()){
+            if(!email.contains("@") && !email.contains(".")){
+                returnString += "please use a valid email";
+            }
+        }
+        else{
+            returnString += "email needs to be filled out \n";
         }
 
-        return "";
-        // todo: hash password here
-        // todo: send it as json to backend
+        if (username.isEmpty()){
+            returnString += "username needs to be filled out \n";
+        }
+        if(password.isEmpty()){
+            returnString += "password needs to be filled out \n";
+        }
+        if(!isChecked){
+            returnString += "PLEASE ACCEPT OUR PRIVACY POLICY AND COOKIES";
+        }
+
+        if(returnString.isEmpty()){
+            // todo: hash password here
+            // todo: send it as json to backend
+            returnString += "signed up";
+        }
+
+        return returnString;
     }
 
     public void toLogin() {
